b9a845a03f863e7712c532bd2847380376767314
Tauhmant <86077546+Tauhmant@users.noreply.github.com>
2023-09-25 15:45:54 +0200

    Add files via upload

--- zadanie7.java
+++ zadanie7.java
@@ -1,20 +1,24 @@
 /**
 123
 */
+
+import java.util.Scanner;
+
 public class zadanie7
 {
 	public static void main(String[] args)
 	{
 		double v, r, e, s;
-		r = 5;
-		e = 2;
-		s = 1.5;
-		v = (r - 2*e) / s;
+		Scanner input = new Scanner(System.in);
 		System.out.println("V to liczba sadzonekm jakie można umieścic w rzędzie");
-		System.out.println("R to długość rzędu w metrach = " + r);
-		System.out.println("E to ilość meijsca zajmowanego przez okratowanie = " + e);
-		System.out.println("S to obległość między sadzonkami w metrach = " + s);
+		System.out.print("R to długość rzędu w metrach = ");
+		r = input.nextDouble();
+		System.out.print("E to ilość meijsca zajmowanego przez okratowanie = " );
+		e = input.nextDouble();
+		System.out.print("S to obległość między sadzonkami w metrach = " );
+		s = input.nextDouble();
 		System.out.println("V = (R - 2E) / S");
+		v = (r - 2*e) / s;
 		System.out.println(v + " = (" + r + " - " + 2*e + ") / " + s);
 	}
 }
